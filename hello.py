cddebc918f1ed46c447f37543cc4c8d3313ce802
faiza1997 <faizameerahhhh@gmail.com>
2026-01-29 22:00:28 +0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+if a == b:
+  print(f"the number is : {a}")
+else:
+  print(f"the first number is : {a} \nthe second number is : {b}")
